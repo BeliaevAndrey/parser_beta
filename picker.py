9e7ee7d07fdcc04fbaf288e306b0645f9420cbc3
Andrei Beliaev <abbot190414@gmail.com>
2023-06-06 16:14:47 +0300

    Commit 4: picker-file -- example file illustrating a start of process

---
+++ picker.py
@@ -0,0 +1,27 @@
+"""
+
+Example file illustrating call of sites walker.
+
+"""
+from Vac_search import outside_crutch, get_list_of_companies
+from time import sleep
+
+
+def main():
+    luxoft_flag = False
+    count = 5
+    for key, val in outside_crutch(get_list_of_companies()).items():
+        print(key, end=': ', sep='')
+        print(len(val) if val else '\033[31mObtained empty list\033[0m')
+        luxoft_flag = (key == 'luxoft_grb' and val and len(val) < 100)
+    while luxoft_flag and count:
+        print(f'Luxoft will be re-grabbed in 30 seconds. Attemps left: {count}')
+        sleep(30)
+        count -= 1
+        key, val = outside_crutch(['luxoft_grb']).items()
+        print(key, end=': ', sep='')
+        print(len(val) if val else '\033[31mObtained empty list\033[0m')
+
+
+if __name__ == '__main__':
+    main()
